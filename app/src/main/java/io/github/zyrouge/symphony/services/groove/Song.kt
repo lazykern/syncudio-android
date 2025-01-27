80f3a8b53d5be6ad3039f200881ac1e692f26420
Phusit Somboonyingsuk <lazykern@gmail.com>
2025-01-27 19:55:35 +0700

    Fix wrong field case

--- app/src/main/java/io/github/zyrouge/symphony/services/groove/Song.kt
+++ app/src/main/java/io/github/zyrouge/symphony/services/groove/Song.kt
@@ -46,7 +46,7 @@ data class Song(
     val coverFile: String?,
     val uri: Uri,
     val path: String,
-    val blake3_hash: String? = null,
+    val blake3Hash: String? = null,
 ) {
     data class ParseOptions(
         val symphony: Symphony,
@@ -179,7 +179,7 @@ data class Song(
                 coverFile = coverFile,
                 uri = file.uri,
                 path = path.pathString,
-                blake3_hash = null,
+                blake3Hash = null,
             )
         }
 
@@ -256,7 +256,7 @@ data class Song(
                 coverFile = coverFile,
                 uri = file.uri,
                 path = path.pathString,
-                blake3_hash = null,
+                blake3Hash = null,
             )
         }
 
